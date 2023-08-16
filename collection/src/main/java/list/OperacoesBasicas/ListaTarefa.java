fd4bc2b6067b7a72b87ddadec65674b8e1531f6a
antonioluidev <antonioluizti@hotmail.com>
2023-08-16 15:55:40 -0300

    exeercicioslist

---
+++ collection/src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,68 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+  //atributo
+  private List<Tarefa> tarefaList;
+
+  //construtor
+  public ListaTarefa() {
+    this.tarefaList = new ArrayList<>();
+  }
+
+  public void adicionarTarefa(String descricao) {
+    tarefaList.add(new Tarefa(descricao));
+  }
+
+  public void removerTarefa(String descricao) {
+    List<Tarefa> tarefasParaRemover = new ArrayList<>();
+    if (!tarefaList.isEmpty()) {
+      for (Tarefa t : tarefaList) {
+        if (t.getDescricao().equalsIgnoreCase(descricao)) {
+          tarefasParaRemover.add(t);
+        }
+      }
+      tarefaList.removeAll(tarefasParaRemover);
+    } else {
+      System.out.println("A lista está vazia!");
+    }
+//	return descricao;
+  }
+
+  public int obterNumeroTotalTarefas() {
+    return tarefaList.size();
+  }
+
+  public void obterDescricoesTarefas() {
+    if (!tarefaList.isEmpty()) {
+      System.out.println(tarefaList);
+    } else {
+      System.out.println("A lista está vazia!");
+    }
+  }
+
+  public static void main(String[] args) {
+    ListaTarefa listaTarefa = new ListaTarefa();
+    System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+    System.out.println("----------------------------------------");
+   
+    listaTarefa.adicionarTarefa("Tarefa 6");
+    listaTarefa.adicionarTarefa("Tarefa 1");
+    listaTarefa.adicionarTarefa("Tarefa 2");
+    listaTarefa.adicionarTarefa("Tarefa 3");
+    
+    listaTarefa.obterNumeroTotalTarefas();
+    System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+ listaTarefa.removerTarefa("Tarefa 1");
+//  String r= listaTarefa.removerTarefa("Tarefa 1");
+    System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+//    System.out.println("Removido: -> "+ r);
+
+    listaTarefa.obterDescricoesTarefas();
+//    System.out.println("O TESTE É: " + listaTarefa.obterDescricoesTarefas());
+  }
+
+}
